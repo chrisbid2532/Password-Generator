5dfd432be5aea6183d561a7be84101e9d52e153a
Chris Bidwell <chrisbid2532@gmail.com>
2023-07-12 00:47:05 -0400

    Finished Product

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Password-Generator # Password Generator Starter Code
+# Password-Generator
+This is the third homework assignment I completed in my Coding Bootcamp for the University of Minnesotta. I created this repository to show my progress in the bootcamp course for any portential employers I might have. In this assignment, I was tasked with writing the JavaScript code for a password generator website. 
+
+## Installation
+N/A
+
+## Credits
+I used W3 Schools to help further my knowledge of 'if' statements in JavaScript
+https://www.w3schools.com/js/js_if_else.asp
+
+I also used W3 Schools again to help me understand how/where/when to use the proper random code in this assignment
+https://www.w3schools.com/js/js_random.asp
+
+This was my main source which is heavily altered to help meet the acceptance criteria of the assignment
+https://dev.to/code_mystery/random-password-generator-using-javascript-6a\
+
+I was also helped by my peers.
+
+## Usage
+You can visit this website to create a random password
+
+## License
+MIT License
+
+Copyright (c) [2023] [ChristopherBidwell]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
--- script.js
+++ script.js
@@ -10,8 +10,6 @@ var sym = "!@#$%^&*()_+-={}::<>?";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-// The following is all the possible outcomes for the generated password after selecting the prompts
-
 var all = num + sym + lowerCase + upperCase;
 var nonums = sym + lowerCase + upperCase;
 var nosyms = num + lowerCase + upperCase;
@@ -24,24 +22,89 @@ var lowerSym = sym + lowerCase;
 var noLetters = sym + num;
 var onlyLetters = lowerCase + upperCase;
 
-function dropDownBox() {
-  var dropDown = window.prompt("Select Password Length (Must be between 8 and 128 characters)")
-  var numbers = confirm("Select 'OK' for numbers or select 'Cancel' for no numbers")
-  var symbols = confirm("Select 'OK' for symbols or select 'Cancel' for no symbols")
-  var upQuestion = confirm("Select 'OK' for upper case letters or select 'Cancel' for no upper case letters")
-  var lowQuestion = confirm("Select 'OK' for lower case letters or select 'Cancel' for no lower case letters")
+function generatePassword() {
+  var length = window.prompt("Select Password Length (Must be between 8 and 128 characters)");
+  var numbers = confirm("Select 'OK' for numbers or select 'Cancel' for no numbers");
+  var symbols = confirm("Select 'OK' for symbols or select 'Cancel' for no symbols");
+  var upQuestion = confirm("Select 'OK' for upper case letters or select 'Cancel' for no upper case letters");
+  var lowQuestion = confirm("Select 'OK' for lower case letters or select 'Cancel' for no lower case letters");
 
-  var generatedPassword = ""
-  
+  var result = ""
+
+  if (length < 8 || length > 128) {
+    result += "Length must be between 8-128 characters."
+  } else if (numbers == true && symbols == true && lowQuestion == true && upQuestion == true) {
+    for (var i = 0; i < length; i++){
+      result += all.charAt(Math.floor(Math.random() * all.length));
+    }
+  } else if (symbols == true && lowQuestion == true && upQuestion == true) {
+    for (var i = 0; i < length; i++){
+      result += nonums.charAt(Math.floor(Math.random() * nonums.length));
+    }
+  } else if (numbers == true && lowQuestion == true && upQuestion == true) {
+    for (var i = 0; i < length; i++){
+      result += nosyms.charAt(Math.floor(Math.random() * nosyms.length));
+    }
+  } else if (symbols == true && numbers == true && upQuestion == true) {
+    for (var i = 0; i < length; i++){
+      result += noLower.charAt(Math.floor(Math.random() * noLower.length));
+    }
+  } else if (lowQuestion == true && upQuestion == true) {
+    for (var i = 0; i < length; i++){
+      result += onlyLetters.charAt(Math.floor(Math.random() * onlyLetters.length));
+    }
+  } else if (symbols == true && lowQuestion == true && numbers == true) {
+    for (var i = 0; i < length; i++){
+      result += noUpper.charAt(Math.floor(Math.random() * noUpper.length));
+    }
+  } else if (numbers == true && symbols == true) {
+    for (var i = 0; i < length; i++){
+      result += noLetters.charAt(Math.floor(Math.random() * noLetters.length));
+    }
+  } else if (upQuestion == true && numbers == true) {
+    for (var i = 0; i < length; i++){
+      result += upperNum.charAt(Math.floor(Math.random() * upperNum.length));
+    }
+  } else if (lowQuestion == true && numbers == true){
+    for (var i = 0; i < length; i++){
+      result += lowerNum.charAt(Math.floor(Math.random() * lowerNum.length));
+    }
+  } else if (upQuestion == true && symbols == true) {
+    for (var i = 0; i < length; i++){
+      result += upperSym.charAt(Math.floor(Math.random() * upperSym.length));
+    }
+  } else if (lowQuestion == true && symbols == true) {
+    for (var i = 0; i < length; i++){
+      result += lowerSym.charAt(Math.floor(Math.random() * lowerSym.length));
+    }
+  } else if (upQuestion == true) {
+    for (var i = 0; i < length; i++){
+      result += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
+    }
+  } else if (lowQuestion == true) {
+    for (var i = 0; i < length; i++){
+    result += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
+  }
+} else if (numbers == true) {
+  for (var i = 0; i < length; i++){
+  result += num.charAt(Math.floor(Math.random() * num.length));
+}
+} else if (symbols == true) {
+  for (var i = 0; i < length; i++){
+  result += sym.charAt(Math.floor(Math.random() * sym.length));
+}
+} else {
+  result += "Really? You came here to do nothing? Shame."
 }
 
-// Write password to the #password input
+return result
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
